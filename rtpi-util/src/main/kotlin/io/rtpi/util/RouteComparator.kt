192ce9ab2154af60781acac63a666131f6667152
Conor O'Brien <conor.w.ob@gmail.com>
2019-12-12 15:53:58 +0000

    Convert comparator to Kotlin

--- rtpi-util/src/main/kotlin/io/rtpi/util/RouteComparator.kt
+++
@@ -1,9 +0,0 @@
-package io.rtpi.util
-
-import io.rtpi.api.Route
-
-object RouteComparator : Comparator<Route> {
-
-    override fun compare(r1: Route, r2: Route): Int = AlphanumComparator.getInstance().compare(r1.id, r2.id)
-
-}
