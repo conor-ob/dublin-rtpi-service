a8f9fdfc174e75bae4dd17b839b3b73015e02ace
Conor O'Brien <conor.w.ob@gmail.com>
2020-04-09 00:32:58 +0100

    revert

--- rtpi-client/src/main/kotlin/io/rtpi/api/RtpiApi.kt
+++ rtpi-client/src/main/kotlin/io/rtpi/api/RtpiApi.kt
@@ -7,53 +7,53 @@ import retrofit2.http.Query
 interface RtpiApi {
 
     @GET("aircoach/locations")
-    fun getAircoachStops(): Single<List<StopLocation>>
+    fun getAircoachStops(): Single<List<ServiceLocation>>
 
     @GET("aircoach/livedata")
     fun getAircoachLiveData(
         @Query(value = "locationId") stopId: String
-    ): Single<List<PredictionLiveData>>
+    ): Single<List<LiveData>>
 
     @GET("buseireann/locations")
-    fun getBusEireannStops(): Single<List<StopLocation>>
+    fun getBusEireannStops(): Single<List<ServiceLocation>>
 
     @GET("buseireann/livedata")
     fun getBusEireannLiveData(
         @Query(value = "locationId") stopId: String
-    ): Single<List<PredictionLiveData>>
+    ): Single<List<LiveData>>
 
     @GET("dublinbikes/locations")
     fun getDublinBikesDocks(
         @Query(value = "apiKey") apiKey: String
-    ): Single<List<DockLocation>>
+    ): Single<List<ServiceLocation>>
 
     @GET("dublinbikes/livedata")
     fun getDublinBikesLiveData(
         @Query(value = "locationId") dockId: String,
         @Query(value = "apiKey") apiKey: String
-    ): Single<DockLiveData>
+    ): Single<LiveData>
 
     @GET("dublinbus/locations")
-    fun getDublinBusStops(): Single<List<StopLocation>>
+    fun getDublinBusStops(): Single<List<ServiceLocation>>
 
     @GET("dublinbus/livedata")
     fun getDublinBusLiveData(
         @Query(value = "locationId") stopId: String
-    ): Single<List<PredictionLiveData>>
+    ): Single<List<LiveData>>
 
     @GET("irishrail/locations")
-    fun getIrishRailStations(): Single<List<StopLocation>>
+    fun getIrishRailStations(): Single<List<ServiceLocation>>
 
     @GET("irishrail/livedata")
     fun getIrishRailLiveData(
         @Query(value = "locationId") stationId: String
-    ): Single<List<PredictionLiveData>>
+    ): Single<List<LiveData>>
 
     @GET("luas/locations")
-    fun getLuasStops(): Single<List<StopLocation>>
+    fun getLuasStops(): Single<List<ServiceLocation>>
 
     @GET("luas/livedata")
     fun getLuasLiveData(
         @Query(value = "locationId") stopId: String
-    ): Single<List<PredictionLiveData>>
+    ): Single<List<LiveData>>
 }
