35c1a67f675e96412624587a16b03dbbffe6f108
Conor O'Brien <conor.w.ob@gmail.com>
2020-10-23 17:48:51 +0100

    Async cache reloading (#28)

--- rtpi-application/src/main/kotlin/io/rtpi/module/CacheModule.kt
+++ rtpi-application/src/main/kotlin/io/rtpi/module/CacheModule.kt
@@ -4,6 +4,8 @@ import com.authzee.kotlinguice4.KotlinModule
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
 import com.google.inject.Provides
 import com.google.inject.Singleton
 import com.google.inject.name.Named
@@ -23,7 +25,9 @@ import io.rtpi.service.irishrail.IrishRailLiveDataService
 import io.rtpi.service.irishrail.IrishRailStationService
 import io.rtpi.service.luas.LuasLiveDataService
 import io.rtpi.service.luas.LuasStopService
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import org.slf4j.LoggerFactory
 
 class CacheModule : KotlinModule() {
 
@@ -33,15 +37,24 @@ class CacheModule : KotlinModule() {
     fun aircoachStopCache(
         aircoachStopService: AircoachStopService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationCacheSpec))
         .build(
             object : CacheLoader<Service, List<ServiceLocation>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: Service): List<ServiceLocation> {
                     return aircoachStopService.getStops().blockingGet()
+                }
+
+                override fun reload(
+                    key: Service,
+                    oldValue: List<ServiceLocation>
+                ): ListenableFuture<List<ServiceLocation>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -52,15 +65,21 @@ class CacheModule : KotlinModule() {
     fun aircoachLiveDataCache(
         aircoachLiveDataService: AircoachLiveDataService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<String, List<LiveData>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<String, List<LiveData>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataCacheSpec))
         .build(
             object : CacheLoader<String, List<LiveData>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: String): List<LiveData> {
                     return aircoachLiveDataService.getLiveData(key).blockingGet()
+                }
+
+                override fun reload(key: String, oldValue: List<LiveData>): ListenableFuture<List<LiveData>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -71,15 +90,24 @@ class CacheModule : KotlinModule() {
     fun busEireannStopCache(
         busEireannStopService: BusEireannStopService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationCacheSpec))
         .build(
             object : CacheLoader<Service, List<ServiceLocation>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: Service): List<ServiceLocation> {
                     return busEireannStopService.getStops().blockingGet()
+                }
+
+                override fun reload(
+                    key: Service,
+                    oldValue: List<ServiceLocation>
+                ): ListenableFuture<List<ServiceLocation>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -90,15 +118,21 @@ class CacheModule : KotlinModule() {
     fun busEireannLiveDataCache(
         busEireannLiveDataService: BusEireannLiveDataService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<String, List<LiveData>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<String, List<LiveData>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataCacheSpec))
         .build(
             object : CacheLoader<String, List<LiveData>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: String): List<LiveData> {
                     return busEireannLiveDataService.getLiveData(key).blockingGet()
+                }
+
+                override fun reload(key: String, oldValue: List<LiveData>): ListenableFuture<List<LiveData>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -109,15 +143,24 @@ class CacheModule : KotlinModule() {
     fun dublinBikesDockCache(
         dublinBikesDockService: DublinBikesDockService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<String, List<ServiceLocation>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<String, List<ServiceLocation>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationCacheSpec))
         .build(
             object : CacheLoader<String, List<ServiceLocation>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: String): List<ServiceLocation> {
                     return dublinBikesDockService.getDocks(key).blockingGet()
+                }
+
+                override fun reload(
+                    key: String,
+                    oldValue: List<ServiceLocation>
+                ): ListenableFuture<List<ServiceLocation>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -128,15 +171,21 @@ class CacheModule : KotlinModule() {
     fun dublinBikesLiveDataCache(
         dublinBikesLiveDataService: DublinBikesLiveDataService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<Pair<String, String>, LiveData> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<Pair<String, String>, LiveData> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataCacheSpec))
         .build(
             object : CacheLoader<Pair<String, String>, LiveData>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: Pair<String, String>): LiveData {
                     return dublinBikesLiveDataService.getLiveData(key.first, key.second).blockingGet()
+                }
+
+                override fun reload(key: Pair<String, String>, oldValue: LiveData): ListenableFuture<LiveData> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -147,15 +196,30 @@ class CacheModule : KotlinModule() {
     fun dublinBusStopCache(
         dublinBusStopService: DublinBusStopService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationCacheSpec))
         .build(
             object : CacheLoader<Service, List<ServiceLocation>>() {
+
+                private val logger = LoggerFactory.getLogger(CacheModule::class.java)
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: Service): List<ServiceLocation> {
-                    return dublinBusStopService.getStops().blockingGet()
+                    logger.info("loading $key")
+                    val result = dublinBusStopService.getStops().blockingGet()
+                    logger.info("finished loading $key")
+                    return result
+                }
+
+                override fun reload(
+                    key: Service,
+                    oldValue: List<ServiceLocation>
+                ): ListenableFuture<List<ServiceLocation>> {
+                    logger.info("reloading $key")
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -166,15 +230,21 @@ class CacheModule : KotlinModule() {
     fun dublinBusLiveDataCache(
         dublinBusLiveDataService: DublinBusLiveDataService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<String, List<LiveData>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<String, List<LiveData>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataCacheSpec))
         .build(
             object : CacheLoader<String, List<LiveData>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: String): List<LiveData> {
                     return dublinBusLiveDataService.getLiveData(key).blockingGet()
+                }
+
+                override fun reload(key: String, oldValue: List<LiveData>): ListenableFuture<List<LiveData>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -185,15 +255,24 @@ class CacheModule : KotlinModule() {
     fun irishRailStationCache(
         irishRailStationService: IrishRailStationService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationCacheSpec))
         .build(
             object : CacheLoader<Service, List<ServiceLocation>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: Service): List<ServiceLocation> {
                     return irishRailStationService.getStations().blockingGet()
+                }
+
+                override fun reload(
+                    key: Service,
+                    oldValue: List<ServiceLocation>
+                ): ListenableFuture<List<ServiceLocation>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -204,15 +283,21 @@ class CacheModule : KotlinModule() {
     fun irishRailLiveDataCache(
         irishRailLiveDataService: IrishRailLiveDataService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<String, List<LiveData>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<String, List<LiveData>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataCacheSpec))
         .build(
             object : CacheLoader<String, List<LiveData>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: String): List<LiveData> {
                     return irishRailLiveDataService.getLiveData(key).blockingGet()
+                }
+
+                override fun reload(key: String, oldValue: List<LiveData>): ListenableFuture<List<LiveData>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -223,15 +308,24 @@ class CacheModule : KotlinModule() {
     fun luasStopCache(
         luasStopService: LuasStopService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<Service, List<ServiceLocation>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.serviceLocationCacheSpec))
         .build(
             object : CacheLoader<Service, List<ServiceLocation>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: Service): List<ServiceLocation> {
                     return luasStopService.getStops().blockingGet()
+                }
+
+                override fun reload(
+                    key: Service,
+                    oldValue: List<ServiceLocation>
+                ): ListenableFuture<List<ServiceLocation>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
@@ -242,15 +336,21 @@ class CacheModule : KotlinModule() {
     fun luasLiveDataCache(
         luasLiveDataService: LuasLiveDataService,
         rtpiServiceConfiguration: RtpiServiceConfiguration
-    ): LoadingCache<String, List<LiveData>> = CacheBuilder.newBuilder()
-        .expireAfterWrite(
-            requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataExpiry?.seconds),
-            TimeUnit.SECONDS
-        )
+    ): LoadingCache<String, List<LiveData>> = CacheBuilder
+        .from(requireNotNull(rtpiServiceConfiguration.cacheConfiguration.liveDataCacheSpec))
         .build(
             object : CacheLoader<String, List<LiveData>>() {
+
+                private val cacheReloadingExecutorService = MoreExecutors.listeningDecorator(
+                    Executors.newFixedThreadPool(10)
+                )
+
                 override fun load(key: String): List<LiveData> {
                     return luasLiveDataService.getLiveData(key).blockingGet()
+                }
+
+                override fun reload(key: String, oldValue: List<LiveData>): ListenableFuture<List<LiveData>> {
+                    return cacheReloadingExecutorService.submit(Callable { load(key) })
                 }
             }
         )
